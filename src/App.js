d310132d73c198122786d22672864e858492a213
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-03 11:23:36 +0700

    page preparation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import home from './pages/home/home';
+import projects from './pages/projects/projects';
+import blog from './pages/blog/blog';
+import about from './pages/about';
+import contact from './pages/contacts/contact';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/">
+          <home />
+        </Route>
+        <Route path="/projects">
+          <projects />
+        </Route>
+        <Route path="/blog">
+          <blog />
+        </Route>
+        <Route path="/about">
+          <about />
+        </Route>
+        <Route path="/contact-me">
+          <contacts />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
