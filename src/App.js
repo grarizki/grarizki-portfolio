1eb044ded5fe480b5cdd739e95f70514e07383a5
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-04 13:25:26 +0700

    test tailwind

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Home from './pages/home/Home';
-import Projects from './pages/projects/Projects';
-import Blog from './pages/blog/Blog';
-import About from './pages/about';
-import Contact from './pages/contacts/Contact';
+// import Home from './pages/home/Home';
+// import Projects from './pages/projects/Projects';
+// import Blog from './pages/blog/Blog';
+// import About from './pages/about';
+// import Contact from './pages/contacts/Contact';
+// import Navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      <h1 className="flex mx-auto text-2xl text-red-900 "> INI PAKE TAILWIND</h1>
+      {/* <Switch>
         <Route path="/">
           <Home />
         </Route>
@@ -27,7 +29,7 @@ function App() {
         <Route path="/contact-me">
           <Contact />
         </Route>
-      </Switch>
+      </Switch> */}
     </div>
   );
 }
